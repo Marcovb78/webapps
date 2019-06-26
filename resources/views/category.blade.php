4b7bc2ba5a09608c437e4e75904f76a44b5c1ec8
MelvinBrink <48125702+MelvinBrink@users.noreply.github.com>
2019-06-26 12:26:26 +0200

    -

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,45 +3,37 @@
     @section('content')
 
     <header class="masthead" style="height: 40vh;">
-        @include('navbar')
-        <div class="container d-flex justify-content-center flex-column text-center text-light">
-            <div class="test">
-                <span style="font-size:60px;" class="welcome">Alle projecten</span>
+            @include('navbar')
+            <div class="container d-flex justify-content-center flex-column text-center text-light">
+                <div class="test">
+                    <span style="font-size:60px;" class="welcome">Alle projecten</span>
+                </div>
             </div>
-        </div>
-    </header>
-
-    <div class="wrapper">
-        @include('sidebar')
-        <div class="sidebar-open"><i class="fas fa-bars"></i></div>
-            <div class="row">
-                <!-- Hier laat je alle projecten zien -->
+        </header>
+    
+        <div class="wrapper">
+            @include('sidebar')
+            <div class="sidebar-open"><i class="fas fa-bars"></i></div>
+            <div class="fill-height">
+                <div class="d-flex justify-content-center flex-wrap p-5">
                     @foreach ($projects as $project)
-                        <a href="{{ url('projects/' . $project->id )}}" style="text-decoration: none;">
-                            <div class="col-md-6">
-                                <div class="uk-card uk-card-default uk-card-hover" style="margin-top: 30px; width:500px;">
-                                    <div class="uk-card-header">
-                                        <h1 class="uk-card-title">{{ $project->title }}</h1>
-                                    </div>
-                                    <div class="uk-card-body">
-                                        <div class="uk-card-badge uk-label">{{ $project->user->name }}</div>
-                                        <p style="color: #000;">{{ $project->description }}</p>
-                                    </div>
-                                    <div class="uk-card-footer">
-                                        <span style="float:right; color: #000;">{{ $project->created_at }}</span>
-                                    </div>
-                                    <a href="{{ url('projects/'.$project->id .'/edit' )}}"><button type="button" class="btn btn-secondary" style="margin: 0 0 15px 15px;">Edit je project</button></a>
-                                </div>
+                    <div class="uk-card uk-card-default uk-card-hover m-3" style="margin-top: 30px; width:300px;">
+                            <div class="uk-card-header">
+                                <h1 class="uk-card-title">{{ $project->title }}</h1>
                             </div>
-                        </a>
+                            <div class="uk-card-body">
+                                <div class="uk-card-badge uk-label">{{ $project->user->name }}</div>
+                                <p style="color: #000;">{{ $project->description }}</p>
+                            </div>
+                            <div class="uk-card-footer">
+                                <span style="float:right; color: #000;">{{ $project->created_at }}</span>
+                            </div>
+                            <a href="{{ url('projects/'.$project->id .'/edit' )}}"><button type="button" class="btn btn-secondary" style="margin: 0 0 15px 15px;">Edit je project</button></a>
+                        </div>
                     @endforeach
                 </div>
             </div>
         </div>
-        <footer class="footer" style="background-color:black; min-height:250px; width:100%; float:left;">
-
-        </footer>
-    </div>
 
 
     @endsection
